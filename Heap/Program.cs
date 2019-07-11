4e816f47b517a207c9db05ee71f45fb1737c03b6
Kenny Wolfenberger <kennyw@terawe.com>
2019-07-11 10:16:14 -0700

    initial commit for heap

---
+++ Heap/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    class MaxBinaryHeap
+    {
+        List<int> values;
+        MaxBinaryHeap(List<int> vals)
+        {
+            this.values = vals;
+        }
+        void Insert(int val)
+        {
+            this.values.Add(val);
+            int valIdx = this.values.Count - 1;
+            int parentIdx = (valIdx - 1) / 2;
+            while (this.values[valIdx] > this.values[parentIdx])
+            {
+                this.values[valIdx] = this.values[parentIdx];
+                this.values[parentIdx] = val;
+                valIdx = parentIdx;
+                parentIdx = (valIdx - 1) / 2;
+                if (valIdx == 0) break;
+            }
+        }
+        int? ExtractMax() // remove largest value and return;
+        {
+            if (this.values.Count == 0) return null;
+            int result = this.values[0];
+            int last = this.values[this.values.Count - 1];
+            this.values[0] = last;
+            this.values.RemoveAt(this.values.Count - 1);
+            int idx = 0;
+            int leftChild = 1;
+            int rightChild = 2;
+            int count = this.values.Count;
+            while (true)
+            {
+                bool swap = false;
+                if (leftChild >= count) break;
+                if (rightChild >= count)
+                {
+                    if (last < this.values[leftChild])
+                    {
+                        this.values[idx] = this.values[leftChild];
+                        this.values[leftChild] = last;
+                        swap = true;
+                    }
+                }
+                if (leftChild < count && rightChild < count)
+                {
+                    if (this.values[leftChild] > this.values[rightChild])
+                    {
+                        if (last < this.values[leftChild])
+                        {
+                            this.values[idx] = this.values[leftChild];
+                            this.values[leftChild] = last;
+                            swap = true;
+                        }
+                    }
+                    else if (this.values[rightChild] > this.values[leftChild])
+                    {
+                        if (last < this.values[rightChild])
+                        {
+                            this.values[idx] = this.values[rightChild];
+                            this.values[rightChild] = last;
+                            swap = true;
+                        }
+                    }
+                }
+                idx = this.values.IndexOf(last);
+                rightChild = (idx * 2) + 2;
+                leftChild = (idx * 2) + 1;
+                if (!swap) break;
+            }
+            return result;
+        }
+        static void Main()
+        {
+            var list = new List<int> { 86, 80, 75, 49, 6, 65, 35, 1, 5, 2, 3, 54 };
+            MaxBinaryHeap heap = new MaxBinaryHeap(list);
+            foreach (var x in heap.values) Console.WriteLine(x);
+            Console.WriteLine("-----");
+            heap.ExtractMax();
+            foreach (var x in heap.values) Console.WriteLine(x);
+            heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax(); heap.ExtractMax();
+            Console.WriteLine("remaining");
+            foreach (var x in heap.values) Console.WriteLine(x);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# _Binary Heap_
+
+#### _C# implementation of Max Binary Heap_
+
+#### By _**Kenny Wolfenberger**_
+
+## Description
+
+_This program implements a Heap in C#. It includes a MaxBinaryHeap class with Insert, ExtractMax functions._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open Heap.sln in IDE like Visual Studio._
+* _By default the program will run with a basic Binary Heap and go through several functions outlined in the Main function._
+
+
+## Support and contact details
+
+_Contact Kenny Wolfenberger - kennywolfenberger@gmail.com_
+
+
+## Technologies Used
+
+* _C# .NET Framework_
+* _Visual Studio_
+
+
+Copyright (c) 2019 **_Kenny Wolfenberger_**
